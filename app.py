4f7436b6983bb87fc829bec97f6f3a6390a7b561
Diego <105861847+Doliver-O@users.noreply.github.com>
2025-08-04 23:09:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def unauthorized(error):
 
 
 if __name__ == "__main__":
-    #serve(app, host='0.0.0.0', port=8080)
-    app.run(debug=True)
+    serve(app, host='0.0.0.0', port=8080)
+    #app.run(debug=True)
